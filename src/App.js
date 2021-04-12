8ee27f78ebe9cfaf9a58c4c7457c3efbe77b7dd5
Chris <chawkins2010@hotmail.com>
2021-04-11 22:12:06 -0500

    material-ui Button added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          startIcon={<SaveIcon />}
+          style={{
+            fontSize: 12,
+            fontWeight: 'bold',
+            backgroundColor: 'green'
+          }} 
+          onClick={() => alert('You have entered coding grounds!')} variant="contained"           
         >
-          Learn React
-        </a>
+          Coding Grounds
+        </Button>
+        <img src={logo} className="App-logo" alt="logo" />        
       </header>
     </div>
   );
